b3fded249205505acd5c26de3a7afbf3349c90c6
Eric Rosetti Lessa <ericrlessa@gmail.com>
2025-06-17 10:37:30 -0400

    wip

--- server/local_test/predict.sh
+++ server/local_test/predict.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-payload=$1
-content=${2:-text/csv}
+#payload=$1
+#content=${2:-text/csv}
 
-curl --data-binary @${payload} -H "Content-Type: ${content}" -v http://localhost:8080/invocations
+#curl --data-binary @${payload} -H "Content-Type: ${content}" -v http://localhost:8080/invocations
+
+curl -X POST http://localhost:8080/invocations \
+  -F "audio=@../../src/f5_tts/infer/examples/basic/basic_ref_en.wav" \
+  -F "text=Hello World." \
+  -F "ref_text=Some call me nature, others call me mother nature."

25239c2fca55bc5b5b8dcfc86b930aeb039e671d
Eric Rosetti Lessa <ericrlessa@gmail.com>
2025-06-25 11:14:22 -0400

    wip

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -29,7 +29,7 @@ variable "sqs_queue_name" {
 
 variable "ecs_instance_type" { 
   type=string 
-  default="t3.xlarge" 
+  default="g4dn.xlarge" 
 }
 
 variable "ecs_cluster_name" { 
@@ -39,7 +39,7 @@ variable "ecs_cluster_name" {
 
 variable "ecs_ami_ssm_param" {
   type = string
-  default = "/aws/service/ecs/optimized-ami/amazon-linux-2/recommended/image_id"
+  default = "/aws/service/ecs/optimized-ami/amazon-linux-2/gpu/recommended/image_id"
 }
 
 variable "generate_audio_function_name" {

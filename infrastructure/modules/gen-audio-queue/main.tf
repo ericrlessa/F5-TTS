372f3b3a123c80b9599e7b1b404d891b9361c5f7
Eric Rosetti Lessa <ericrlessa@gmail.com>
2025-07-31 10:19:55 -0400

    added dlq

--- infrastructure/modules/gen-audio-queue/main.tf
+++ infrastructure/modules/gen-audio-queue/main.tf
@@ -7,7 +7,17 @@ terraform {
   }
 }
 
-# 🔹 Create SQS Queue
+# Dead Letter Queue
+resource "aws_sqs_queue" "gen_audio_dlq" {
+  name = "${var.sqs_queue_name}-dlq"
+}
+
+# Main Queue with DLQ
 resource "aws_sqs_queue" "gen_audio_queue" {
   name = var.sqs_queue_name
+
+  redrive_policy = jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.gen_audio_dlq.arn
+    maxReceiveCount     = 3  # After 3 failed receives, message goes to DLQ
+  })
 }

20794702850f13d1ae1d4de779eaa39ca1905296
Eric Rosetti Lessa <ericrlessa@gmail.com>
2025-08-21 14:01:43 -0400

    wip

--- infrastructure/modules/api-gateway/main.tf
+++ infrastructure/modules/api-gateway/main.tf
@@ -205,24 +205,3 @@ resource "aws_lambda_permission" "allow_apigw_voice" {
   source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/${var.env}/GET/voice"
 }
 
-# Output the API key value (you'll need to retrieve this securely)
-output "api_key_value" {
-  description = "The value of the API key. Store this securely!"
-  value       = aws_api_gateway_api_key.voice_clone_api_key.value
-  sensitive   = true
-}
-
-output "api_key_id" {
-  description = "The ID of the API key"
-  value       = aws_api_gateway_api_key.voice_clone_api_key.id
-}
-
-output "usage_plan_id" {
-  description = "The ID of the usage plan"
-  value       = aws_api_gateway_usage_plan.voice_clone_usage_plan.id
-}
-
-output "api_invoke_url" {
-  description = "The base URL of the API"
-  value       = "https://${aws_api_gateway_rest_api.api.id}.execute-api.${var.region}.amazonaws.com/${aws_api_gateway_stage.stage_env.stage_name}"
-}

36a6b53b4da5e910c44bfa659057a559cad5a64a
Eric Rosetti Lessa <ericrlessa@gmail.com>
2025-10-04 14:56:33 -0400

    wip

--- scaling-controller/app.py
+++ scaling-controller/app.py
@@ -82,7 +82,7 @@ def forward(queue_url, message_data):
         MessageBody=json.dumps(message_data)
     )
 
-def total_pending_messages(queue_url):
+def get_total_pending_messages(queue_url):
     response = sqs.get_queue_attributes(
         QueueUrl=queue_url,
         AttributeNames=[
@@ -123,7 +123,7 @@ def scale_ecs_service(ecs_service, podcast_queue_url, messages_per_instances):
 
     logger.info(f"Verifying service {ecs_service} to scale")
 
-    total_pending_messages = total_pending_messages(podcast_queue_url)
+    total_pending_messages = get_total_pending_messages(podcast_queue_url)
     _, desired_count, _ = count_running_instances(ecs_service)
 
     logger.info(f"Total pending messages: {total_pending_messages}")

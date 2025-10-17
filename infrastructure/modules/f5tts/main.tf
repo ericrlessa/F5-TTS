3b4dc003f894f7e6e793aef7b25fd1f094828b9a
Eric Rosetti Lessa <ericrlessa@gmail.com>
2025-10-17 14:11:30 -0400

    wip

--- infrastructure/modules/f5tts/main.tf
+++ infrastructure/modules/f5tts/main.tf
@@ -248,7 +248,8 @@ resource "aws_iam_policy" "s3_read_access" {
         Action = [
           "s3:GetObject",
           "s3:GetObjectVersion",
-          "s3:ListBucket"
+          "s3:ListBucket",
+          "s3:PutObject"
         ]
         Resource = [
           "arn:aws:s3:::${var.bucket_name}",

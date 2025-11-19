dfac9e5aff4518377faf2c00cfaeac41c6d037cc
Eric Rosetti Lessa <ericrlessa@gmail.com>
2025-11-19 09:00:08 -0500

    wip

--- infrastructure/modules/api-gateway-websocket/main.tf
+++ infrastructure/modules/api-gateway-websocket/main.tf
@@ -1,5 +1,5 @@
 resource "aws_apigatewayv2_api" "websocket_api" {
-  name          = "Geniuspod-websocket-api"
+  name          = "Geniuspod-websocket-api-${terraform.workspace}"
   protocol_type = "WEBSOCKET"
   route_selection_expression = "$request.body.action"
   description   = "Scraper Websocket API"
